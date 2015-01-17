17a4f3c2b4588bc928c74d9cb3f178ec0ffa5fae
Mike Funk <mike@mikefunk.com>
2015-01-16 18:20:29 -0800

    updated spec with SIF and comments

--- spec/MyMathSpec.js
+++ spec/MyMathSpec.js
@@ -1,7 +1,15 @@
-define(['../src/mymath'], function (mymath) {
-  describe("test suite", function () {
-    it("should add two numbers together", function () {
-      expect(mymath.add(1, 1)).toEqual(2);
+(function () {
+  'use strict';
+  //
+  // requirejs define variable
+  define(['../src/mymath'], function (mymath) {
+    // jasmine test suite
+    describe("test suite", function () {
+      // jasmine test
+      it("should add two numbers together", function () {
+        // jasmine assertion
+        expect(mymath.add(1, 1)).toEqual(2);
+      });
     });
   });
-});
+}());
